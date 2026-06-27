9bf65c85e96e8198744ad63e7d7122af05bcfaf5
rahul <satyajitsahoo45u@gmail.com>
2026-06-27 08:53:13 +0530

    Add Wishes

--- Greet.java
+++ Greet.java
@@ -4,5 +4,6 @@ public class Greet {
     public static void main(String[] args) {
         System.out.println("Hello Everyone");
         System.out.println("Welcome to Java ");
+        System.out.println("Have a Great Day");
 }
 }
