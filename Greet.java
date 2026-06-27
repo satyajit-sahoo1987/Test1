1fb16c0f45e2ce0c6c4cee629e83827747cea3c5
rahul <satyajitsahoo45u@gmail.com>
2026-06-27 08:51:39 +0530

    add Greet to everyone

---
+++ Greet.java
@@ -0,0 +1,8 @@
+package Test1;
+
+public class Greet {
+    public static void main(String[] args) {
+        System.out.println("Hello Everyone");
+        System.out.println("Welcome to Java ");
+}
+}
